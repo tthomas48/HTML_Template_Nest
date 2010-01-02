20c36b727baaa2f7c90c2bb5ec1badf962181870
Tim Thomas <tim@buyplaytix.com>
2010-01-02 15:02:09 +0000

    Tagging the 0.6.0 release

---
+++ test/ParserTest.php
@@ -0,0 +1,231 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Tests expression parsing functionality.
+ *
+ * PHP version 5
+ *
+ * This source file is subject to the New BSD license, That is bundled
+ * with this package in the file LICENSE, and is available through
+ * the world-wide-web at
+ * http://www.opensource.org/licenses/bsd-license.php
+ * If you did not receive a copy of the new BSDlicense and are unable
+ * to obtain it through the world-wide-web, please send a note to
+ * tthomas48@php.net so we can mail you a copy immediately.
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   SVN: $Id$
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @see       HTML_Template_Nest_Parser
+ * @since     File available since Release 1.0
+ */
+require_once 'HTML/Template/Nest/Parser.php';
+require_once 'PHPUnit/Framework/TestCase.php';
+/**
+ * Tests expression parsing functionality.
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @see       HTML_Template_Nest_Parser
+ * @since     Class available since Release 1.0.0
+ */
+class HTML_Template_Nest_ParserTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test single variable token parsing
+     * 
+     * @return null
+     */
+    public function testSimpleTokens()
+    {
+        $parser = new HTML_Template_Nest_Parser();
+        $output = $parser->parse('${some_var}');
+        $this->assertEquals("<?php echo \$_o(\$p, 'some_var')?>", $output);
+        $parser->registerVariable('my_var');
+        $output = $parser->parse('${my_var}');
+        $this->assertEquals("<?php echo \$_os(\$my_var)?>", $output);
+    }
+
+    /**
+     * Test variables that call object methods or members
+     * 
+     * @return null
+     */
+    public function testObjectVariables()
+    {
+
+        $parser = new HTML_Template_Nest_Parser();
+        $output = $parser->parse('${some_var->foo}');
+        $this->assertEquals("<?php echo \$_o(\$p, 'some_var')->foo?>", $output);
+        $output = $parser->parse('${some_var->FOO}');
+        $this->assertEquals("<?php echo \$_o(\$p, 'some_var')->FOO?>", $output);
+        $output = $parser->parse('${some_var->foo(bar,bin, baz)}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var')->foo(\$_o(\$p, 'bar'),\$_o(\$p, 'bin'),\$_o(\$p, 'baz'))?>", 
+            $output
+        );
+        
+        $output = $parser->parse(
+            '${some_var->foo(bar,"bin", baz,  \'boo biscuits\'  )}'
+        );
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var')->foo(\$_o(\$p, 'bar')," .
+            "\"bin\",\$_o(\$p, 'baz'),'boo biscuits')?>", 
+            $output
+        );
+
+        $output = $parser->parse(
+            '${some_var->foo(bar,"bin", baz,  \'boo biscuit\\\'s brother\'  )}'
+        );
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var')->foo(\$_o(\$p, 'bar'),\"bin\"" .
+            ",\$_o(\$p, 'baz'),'boo biscuit\'s brother')?>", 
+            $output
+        );
+        
+        // nested methos
+        $output = $parser->parse(
+            '${some_var->foo(bar, "bin")->bar()->bob("bo\"oo\"",bin)}'
+        );
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var')->foo(\$_o(\$p, 'bar'),\"bin\")" .
+            "->bar()->bob(\"bo\\\"oo\\\"\",\$_o(\$p, 'bin'))?>", 
+            $output
+        );
+    }
+    
+    /**
+     * Test arrays
+     * 
+     * @return null
+     */        
+    public function testArrays()
+    {
+        $parser = new HTML_Template_Nest_Parser();
+        $output = $parser->parse('${some_var[0]}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var')[0]?>", 
+            $output
+        );
+        $output = $parser->parse('${some_var["value"]}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var')[\"value\"]?>", 
+            $output
+        );        
+        $output = $parser->parse('${some_var["value"][0]}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var')[\"value\"][0]?>", 
+            $output
+        );        
+    }
+    
+    /**
+     * Test operators and parenthesis in expresions
+     * 
+     * @return null
+     */    
+    public function testOperators()
+    {
+        $parser = new HTML_Template_Nest_Parser();
+        $output = $parser->parse('${some_var + some_other_var}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var') + \$_o(\$p, 'some_other_var')?>", 
+            $output
+        );
+        $output = $parser->parse('${some_var < some_other_var}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var') < \$_o(\$p, 'some_other_var')?>", $output
+        );
+        $output = $parser->parse('${some_var <= some_other_var}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var') <= \$_o(\$p, 'some_other_var')?>", $output
+        );
+        $output = $parser->parse('${some_var > some_other_var}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var') > \$_o(\$p, 'some_other_var')?>", $output
+        );
+        $output = $parser->parse('${some_var >= some_other_var}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var') >= \$_o(\$p, 'some_other_var')?>", $output
+        );
+        $output = $parser->parse('${some_var == some_other_var}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var') == \$_o(\$p, 'some_other_var')?>", $output
+        );
+        $output = $parser->parse('${some_var != some_other_var}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var') != \$_o(\$p, 'some_other_var')?>", $output
+        );
+        $output = $parser->parse('${(some_var % some_other_var) == 0}');
+        $this->assertEquals(
+            "<?php echo (\$_o(\$p, 'some_var') % \$_o(\$p, 'some_other_var')) == 0?>", $output
+        );
+        $output = $parser->parse('${some_var / some_other_var}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var') / \$_o(\$p, 'some_other_var')?>", $output
+        );
+        $output = $parser->parse('${some_var * some_other_var}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var') * \$_o(\$p, 'some_other_var')?>", $output
+        );
+        $output = $parser->parse('${some_var - some_other_var}');
+        $this->assertEquals(
+            "<?php echo \$_o(\$p, 'some_var') - \$_o(\$p, 'some_other_var')?>", $output
+        );
+        $output = $parser->parse('${(a + b + c) * (d - 3)}');
+        $this->assertEquals(
+            "<?php echo (\$_o(\$p, 'a') + \$_o(\$p, 'b') + \$_o(\$p, 'c')) * (\$_o(\$p, 'd') - 3)?>", 
+            $output
+        );
+        $output = $parser->parse('${(a + b + c) && (d - 3)}');
+        $this->assertEquals(
+            "<?php echo (\$_o(\$p, 'a') + \$_o(\$p, 'b') + \$_o(\$p, 'c')) && (\$_o(\$p, 'd') - 3)?>", 
+            $output
+        );
+        $output = $parser->parse('${(a + b + c) || (d - 3)}');
+        $this->assertEquals(
+            "<?php echo (\$_o(\$p, 'a') + \$_o(\$p, 'b') + \$_o(\$p, 'c')) || (\$_o(\$p, 'd') - 3)?>",
+            $output
+        );
+        $output = $parser->parse('${foo == \'a\'}');
+        $this->assertEquals("<?php echo \$_o(\$p, 'foo') == 'a'?>", $output);
+        $output = $parser->parse('${foo == "b"}');
+        $this->assertEquals("<?php echo \$_o(\$p, 'foo') == \"b\"?>", $output);        
+        
+        $output = $parser->parse('${(foo == "b" ? "black" : "red")}');
+        $this->assertEquals(
+            "<?php echo (\$_o(\$p, 'foo') == \"b\" ? \"black\" : \"red\")?>", 
+            $output
+        );        
+    }
+    
+    /**
+     * Test calling external functions with fn: syntax.
+     * 
+     * @return null
+     */
+    public function testFunctions()
+    {
+        $parser = new HTML_Template_Nest_Parser();
+        $output = $parser->parse('${fn:count(myarray)}');
+        $this->assertEquals("<?php echo count(\$_o(\$p, 'myarray'))?>", $output);
+        $output = $parser->parse('${fn:str_replace(\'foo\', "bar", myarray)}');
+        $this->assertEquals(
+            "<?php echo str_replace('foo',\"bar\",\$_o(\$p, 'myarray'))?>",
+            $output
+        );
+        
+        
+    }
+}
---
+++ test/TestTaglib.php
@@ -0,0 +1,206 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Custom tag library for testing.
+ *
+ * PHP version 5
+ *
+ * This source file is subject to the New BSD license, That is bundled
+ * with this package in the file LICENSE, and is available through
+ * the world-wide-web at
+ * http://www.opensource.org/licenses/bsd-license.php
+ * If you did not receive a copy of the new BSDlicense and are unable
+ * to obtain it through the world-wide-web, please send a note to
+ * tthomas48@php.net so we can mail you a copy immediately.
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   SVN: $Id$
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @see       HTML_Template_Nest_Taglib
+ * @since     File available since Release 1.0
+ */
+require_once 'HTML/Template/Nest/Tag.php';
+require_once 'HTML/Template/Nest/TagFile.php';
+require_once 'HTML/Template/Nest/Taglib.php';
+/**
+ * Custom tag library for testing.
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @see       HTML_Template_Nest_Taglib
+ * @since     Class available since Release 1.0.0
+ */
+class TestTaglib extends HTML_Template_Nest_Taglib
+{
+    protected $tags = array(
+        "testTagFile" => "TestTaglib_TestTagFile", 
+        "wrappableTagFile" => "TestTaglib_WrappableTagFile",
+        "wrappableWithAttributes" => "TestTaglib_WrappableWithAttributesTagFile",
+        "badXml" => "TestTaglib_BadXmlTagFile",
+        "nested" => "TestTaglib_NestedTagFile",
+        "roundedContainer" => "TestTaglib_RoundedContainer",
+        "body" => "TestTaglib_Body",
+    );
+}
+
+/**
+ * A simple tag based upon a tagfile.
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @see       HTML_Template_Nest_TagFile
+ * @since     Class available since Release 1.0.0
+ */
+class TestTaglib_TestTagFile extends HTML_Template_Nest_TagFile
+{
+    /**
+     * gets the tag's filename
+     * 
+     * @see HTML_Template_Nest_Tagfile::getTagFilename()
+     * @return string filename
+     */
+    public function getTagFilename() 
+    {
+        return dirname(__FILE__) . "/templates/tagfile.tmpl";
+    } 
+}
+
+/**
+ * A tag based upon a tagfile that can contain body content.
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @see       HTML_Template_Nest_TagFile
+ * @since     Class available since Release 1.0.0
+ */
+class TestTaglib_WrappableTagFile extends HTML_Template_Nest_TagFile
+{
+    /**
+     * gets the tag's filename
+     * 
+     * @see HTML_Template_Nest_Tagfile::getTagFilename()
+     * @return string filename
+     */
+    public function getTagFilename() 
+    {
+        return dirname(__FILE__) . "/templates/wrappable_tagfile.tmpl";
+    } 
+}
+
+/**
+ * A tag based upon a tagfile that can contain bad xml.
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @see       HTML_Template_Nest_TagFile
+ * @since     Class available since Release 1.0.0
+ */
+class TestTaglib_BadXmlTagFile extends HTML_Template_Nest_TagFile
+{
+    /**
+     * gets the tag's filename
+     * 
+     * @see HTML_Template_Nest_Tagfile::getTagFilename()
+     * @return string filename
+     */    
+    public function getTagFilename() 
+    {
+        return dirname(__FILE__) . "/templates/badxml_tagfile.tmpl";
+    }
+}
+
+/**
+ * A tag that contains a nested tag in the same tag library
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @see       HTML_Template_Nest_TagFile
+ * @since     Class available since Release 1.0.0
+ */
+class TestTaglib_NestedTagFile extends HTML_Template_Nest_TagFile
+{
+    /**
+     * gets the tag's filename
+     * 
+     * @see HTML_Template_Nest_Tagfile::getTagFilename()
+     * @return string filename
+     */
+    public function getTagFilename() 
+    {
+        return dirname(__FILE__) . "/templates/nested_tagfile.tmpl";
+    }
+}
+
+class TestTaglib_Body extends HTML_Template_Nest_TagFile
+{
+    /**
+     * gets the tag's filename
+     * 
+     * @see HTML_Template_Nest_Tagfile::getTagFilename()
+     * @return string filename
+     */
+    public function getTagFilename() {
+        return dirname(__FILE__) . "/templates/body.tmpl";
+    }    
+}
+class TestTaglib_RoundedContainer extends HTML_Template_Nest_TagFile
+{
+    /**
+     * gets the tag's filename
+     * 
+     * @see HTML_Template_Nest_Tagfile::getTagFilename()
+     * @return string filename
+     */
+    public function getTagFilename() {
+        return dirname(__FILE__) . "/templates/roundedcontainer.tmpl";
+    }
+
+}
+
+class TestTaglib_WrappableWithAttributesTagFile 
+    extends HTML_Template_Nest_TagFile
+{
+    protected $declaredAttributes = array(
+        "attribute1", "attribute2", "attribute3"
+    );
+    /**
+     * gets the tag's filename
+     * 
+     * @see HTML_Template_Nest_Tagfile::getTagFilename()
+     * @return string filename
+     */
+    public function getTagFilename() 
+    {
+        return dirname(__FILE__) . "/templates/wrappable_tagfile.tmpl";
+    } 
+}

6519ce5908de1f4753aed928b0b0d3dde000085e
Tim Thomas <tim@buyplaytix.com>
2010-01-11 15:27:16 +0000

    foreach with associative array had parameters backwards

--- HTML/Template/Nest/Taglib/Standard.php
+++ HTML/Template/Nest/Taglib/Standard.php
@@ -243,7 +243,7 @@ class HTML_Template_Nest_Taglib_Standard_ForeachTag extends HTML_Template_Nest_T
         $key = $this->getOptionalAttribute("key");
         if (!empty($key)) {
             $this->registerVariable($key);
-            $output .= "foreach($items as \$$var => \$$key) {\n";
+            $output .= "foreach($items as \$$key => \$$var) {\n";
             return $this->wrapOutput($output);
         }
         $output .= "foreach($items as \$$var) {\n";

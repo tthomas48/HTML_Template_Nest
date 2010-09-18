b8463b9919f14177dc4c2232683fb9304a6b0377
Tim Thomas <tim@buyplaytix.com>
2010-09-18 15:29:56 +0000

    Tagging the 1.3.3 release

---
+++ HTML/Template/Nest/CompilerException.php
@@ -0,0 +1,71 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Exception thrown by the nest compiler
+ *
+ * PHP version 5
+ *
+ * This source file is subject to the New BSD license, That is bundled
+ * with this package in the file LICENSE, and is available through
+ * the world-wide-web at
+ * http://www.opensource.org/licenses/bsd-license.php
+ * If you did not receive a copy of the new BSDlicense and are unable
+ * to obtain it through the world-wide-web, please send a note to
+ * tthomas48@php.net so we can mail you a copy immediately.
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   SVN: $Id$
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @since     File available since Release 1.0
+ */
+/**
+ * Exception thrown by the nest compiler
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @since     Class available since Release 1.0.0
+ */
+class HTML_Template_Nest_CompilerException extends Exception
+{
+    /**
+     * Constructor
+     *
+     * @param string  $message the error message
+     * @param DomNode $node    the node where the error occurred
+     *
+     * @return HTML_Template_Nest_ParseException
+     */
+    public function __construct($message, $node = null)
+    {
+        // only available in php >= 5.3
+        if($node != null && method_exists($node, "getLineNo")) {
+            
+            $lineNo = $node->getLineNo();
+            $message .= " on line " . $lineNo. ":\n";
+            
+            $originalDoc = explode("\n", $node->ownerDocument->saveXml($node->ownerDocument));
+            
+            $message .= "...\n";
+            if($node->getLineNo() - 1 >= 0) {
+                $message .= ($lineNo - 1) . ":" . $originalDoc[$lineNo - 1] . "\n";
+            }
+            $message .= $node->getLineNo() . ":" . $originalDoc[$lineNo] . "\n";
+            if($node->getLineNo() + 1 < count($originalDoc)) {
+                $message .= ($lineNo + 1) . ":" . $originalDoc[$lineNo + 1] . "\n";
+            }
+            $message .= "...\n";
+            
+        }
+        parent::__construct($message);
+    }
+}
---
+++ HTML/Template/Nest/ParseException.php
@@ -0,0 +1,75 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Exception thrown by the nest expression parser
+ *
+ * PHP version 5
+ *
+ * This source file is subject to the New BSD license, That is bundled
+ * with this package in the file LICENSE, and is available through
+ * the world-wide-web at
+ * http://www.opensource.org/licenses/bsd-license.php
+ * If you did not receive a copy of the new BSDlicense and are unable
+ * to obtain it through the world-wide-web, please send a note to
+ * tthomas48@php.net so we can mail you a copy immediately.
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   SVN: $Id$
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @see       HTML_Template_Nest_Parser
+ * @since     File available since Release 1.0
+ */
+/**
+ * Exception thrown by the nest expression parser
+ *
+ * @category  HTML_Template
+ * @package   Nest
+ * @author    Tim Thomas <tthomas48@php.net>
+ * @copyright 2009 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/HTML_Template_Nest
+ * @see       HTML_Template_Nest_Parser 
+ * @since     Class available since Release 1.0.0
+ */
+class HTML_Template_Nest_ParseException extends Exception
+{
+    private $expression;
+    private $node;
+    /**
+     * Constructor
+     *
+     * @param string  $message the error message
+     * @param DomNode $node    the node where the error occurred
+     *
+     * @return HTML_Template_Nest_ParseException
+     */
+    public function __construct($message, $node = null)
+    {
+        // TODO: in php 5.3 we can print line number $node->getLineNo()
+        parent::__construct($message);
+        $this->node = $node;
+    }
+    
+    public function setExpression($expression) {
+        $this->expression = $expression;
+    }
+
+    public function getNode() {
+        return $this->node;
+    }
+
+    public function __toString() {
+        // only available in php >= 5.3
+        if($this->node != null && method_exists($this->node, "getLineNo")) {
+            
+            return "";
+        }
+        return "Error parsing " . $this->expression . ":\n" . $this->getMessage(); 
+    }
+}
